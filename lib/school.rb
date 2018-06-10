96c2bad8272a27e857ad97c32d6f4a0056b4614c
Lorena <lorena.giddens@outlook.com>
2018-06-10 14:23:06 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,27 +1,33 @@
 class School
 
- def initialize(school_name)
+  def initialize(school_name)
    @school_name = school_name
    # has an empty roster when initialized
    @roster = {}
- end
+  end
 
- def roster
+  def roster
    @roster
- end
+  end
 
- def add_student(students_name, grade)
+  def add_student(students_name, grade)
    # 1: first, create the new key and point it to an empty array
    # also, adds multiple students to a class
    # and adds students to different grades
    roster[grade] ||= []
    # 2: now can push the new value into that array
    roster[grade] << students_name
- end
+  end
 
- def grade(grade)
+  def grade(grade)
    # is able to retrieve students from a grade
    roster[grade]
- end
+  end
+
+  def sort
+    roster.each do |grade, student|
+      sorted[grade] = student.sort
+    end 
+  end
 
 end
