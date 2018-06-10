115e6a08108fcb25e94988426eecd2355bba40e1
Lorena <lorena.giddens@outlook.com>
2018-06-10 14:04:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,8 @@ class School
 
  def add_student(students_name, grade)
    # first, create the new key and point it to an empty array
-   roster[grade] = [] || != nil
+   # adds multiple students to a class
+   roster[grade] ||= []
    # now can push the new value into that array
    roster[grade] << students_name
  end
